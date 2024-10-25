cf2a387120814f792cd3de5a1238a7ab7f421520
dienpq <quang.dien@blockodyssey.io>
2024-10-25 11:21:54 +0700

    feat: update loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 import { Layout } from '@/components/layouts';
 import { Home } from '@/components/modules';
 
-export default async function HomePage() {
-  return (
+import Loading from './loading';
+
+export default function HomePage() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  return !loading ? (
     <Layout>
       <Home />
     </Layout>
+  ) : (
+    <Loading />
   );
 }
